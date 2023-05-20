6465eb391ffa265cbd9f3a22b6802331d7677fd1
noahcoskun <noahcoskun@outlook.com>
2023-05-20 14:37:09 -0500

    Configure Express

---
+++ api/index.js
@@ -0,0 +1,4 @@
+const express = require('express');
+// Creates an Express application. The express() function is a top-level function exported by the express module.
+// Express.js is the most popular web framework for Node.js. It is designed for building web applications and APIs and has been called the de facto standard server framework for Node.js.
+const app = express();
